b9857fac844b33750aae867b30dea822c527b521
VINICIUS CARVALHO DE SOUZA <viniciuscarvalhomuay@gmail.com>
2023-06-15 19:16:18 -0300

    Criando sessão shows

---
+++ src/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll("[data-tab-button]");
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function (botao) {
+      const abaAlvo = botao.target.dataset.tabButton;
+      const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+      escondeTodasAbas();
+      removeButtonAtivo();
+      aba.classList.add("shows__list--is-active");
+      botao.target.classList.toggle("shows__tabs__button--is-active");
+    });
+  }
+});
+
+function removeButtonAtivo() {
+  const buttons = document.querySelectorAll("[data-tab-button]");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("shows__tabs__button--is-active");
+  }
+}
+
+function escondeTodasAbas() {
+  const tabsContainer = document.querySelectorAll("[data-tab-id]");
+  for (let i = 0; i < tabsContainer.length; i++) {
+    tabsContainer[i].classList.remove("shows__list--is-active");
+  }
+}
